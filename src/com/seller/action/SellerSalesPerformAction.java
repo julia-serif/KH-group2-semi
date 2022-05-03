d4ef0d080f1aac8fce018d84642333f5daa74a49
julia-serif <juliainserif@gmail.com>
2022-05-03 16:10:56 +0900

    merge 오류 수정 1차

---
+++ src/com/seller/action/SellerSalesPerformAction.java
@@ -0,0 +1,36 @@
+package com.seller.action;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.shop.controller.Action;
+import com.shop.controller.ActionForward;
+import com.shop.model.OrderDAO;
+import com.shop.model.OrderDTO;
+
+public class SellerSalesPerformAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// 판매자의 id에 해당하는 주문 목록을 조회하여 주문 내역 조회 페이지로 이동시키는 비지니스 로직.
+
+		String seller_id = request.getParameter("seller_id").trim();
+		
+		OrderDAO dao = OrderDAO.getInstance();
+		
+		List<OrderDTO> list = dao.getProductList(seller_id);
+		
+		request.setAttribute("productList", list);
+		
+		ActionForward forward = new ActionForward();
+		
+		forward.setRedirect(false);
+		forward.setPath("seller/sales_performance.jsp");
+		
+		return forward;
+	}
+
+}
