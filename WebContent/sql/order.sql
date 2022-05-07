a9e4c330ac4135f201447d39c05eeac014596e41
julia-serif <juliainserif@gmail.com>
2022-05-08 00:19:06 +0900

    order.sql 판매자 컬럼 다른 테이블로 이동

--- WebContent/sql/order.sql
+++ WebContent/sql/order.sql
@@ -11,8 +11,7 @@ create table ks_order(
 	order_date date,									--주문 날짜
 	recipient varchar2(50) not null,					--수령인(이름)
 	recipient_phone varchar2(20),						--수령인 전화번호
-	address varchar2(30) not null,						--배송 주소
-	seller_id varchar2(20) references shop_user(user_id)--판매자
+	address varchar2(30) not null						--배송 주소
 );
 
 create table ks_product_order(
@@ -22,6 +21,7 @@ create table ks_product_order(
 	product_quantity number(10) not null,					--주문 수량
 	product_price number(10) not null,						--주문 금액	
 	expected_date date,										--수령 예상 날짜
-	order_status varchar2(30) not null						--배송 상태
+	order_status varchar2(30) not null,						--배송 상태
+	seller_id varchar2(20) references shop_user(user_id)	--판매자
 );
 
