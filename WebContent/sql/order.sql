d7194f73acacc555d56f4626c3c40b28844dc520
julia-serif <juliainserif@gmail.com>
2022-05-08 13:30:53 +0900

    order_no varchar2(100)으로 변경

--- WebContent/sql/order.sql
+++ WebContent/sql/order.sql
@@ -6,7 +6,7 @@ drop table ks_seller;
 
 
 create table ks_order(
-	order_no number(10) primary key,					--주문 번호
+	order_no varchar2(100) primary key,					--주문 번호
 	user_id varchar2(20) references shop_user(user_id),	--주문자(회원)
 	order_date date,									--주문 날짜
 	recipient varchar2(50) not null,					--수령인(이름)
@@ -16,7 +16,7 @@ create table ks_order(
 
 create table ks_product_order(
 	product_order_no number(10) primary key,				--상품 주문 번호(각각 상품마다)
-	order_no number(10) references ks_order(order_no),		--주문 번호
+	order_no varchar2(100) references ks_order(order_no),	--주문 번호
 	product_no number(10) references ks_product(pno),		--상품 번호
 	product_quantity number(10) not null,					--주문 수량
 	product_price number(10) not null,						--주문 금액	
