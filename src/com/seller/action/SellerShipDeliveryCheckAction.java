e3b0decfe60639fe5aaef301f3c4bcfcb9e0dc17
julia-serif <juliainserif@gmail.com>
2022-05-09 16:56:21 +0900

    배송 관리 밑 서브페이지들 추가에 따른 mapping 및 action 위치 변경

--- src/com/seller/action/SellerShipDeliveryCheckAction.java
+++ src/com/seller/action/SellerShipDeliveryCheckAction.java
@@ -11,11 +11,11 @@ import com.shop.controller.ActionForward;
 import com.shop.model.OrderDAO;
 import com.shop.model.OrderDTO;
 
-public class SellerShipNewOrderListAction implements Action {
+public class SellerShipDeliveryCheckAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// 판매자의 id에 해당하는 주문 목록을 조회하여 신규 주문 확인 페이지로 이동시키는 비지니스 로직.
+		// 판매자의 id에 해당하는 주문 목록을 조회하여 배송 상태 확인 페이지로 이동시키는 비지니스 로직.
 
 		String user_id = (String) request.getSession().getAttribute("user_id");
 		
@@ -28,7 +28,7 @@ public class SellerShipNewOrderListAction implements Action {
 		ActionForward forward = new ActionForward();
 		
 		forward.setRedirect(false);
-		forward.setPath("seller/ship_new_order_list.jsp");
+		forward.setPath("seller/ship_delivery_check.jsp");
 		
 		return forward;
 	}
