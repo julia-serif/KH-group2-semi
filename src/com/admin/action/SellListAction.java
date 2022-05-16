1b41fde5b756db5a1d91bdacec53f5b1c16a06e6
wcycde96 <68499034+wcycde96@users.noreply.github.com>
2022-05-16 11:52:02 +0900

    판매 내역 액션 수정
    판매 내역 액션 수정

--- src/com/admin/action/SellListAction.java
+++ src/com/admin/action/SellListAction.java
@@ -1,10 +1,14 @@
 package com.admin.action;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+import com.seller.model.ProductOrderDAO;
+import com.seller.model.ProductOrderDTO;
 import com.shop.controller.Action;
 import com.shop.controller.ActionForward;
 
@@ -12,8 +16,19 @@ public class SellListAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ProductOrderDAO dao=ProductOrderDAO.getInstance();
+		
+		List<ProductOrderDTO> list =dao.getAllList();
+		
+		request.setAttribute("List", list);
+		ActionForward forward = new ActionForward();
+		
+		forward.setRedirect(false);
+		forward.setPath("admin/admin_sell_list.jsp");
+		
+		return forward;
+		
 	}
 
 }
